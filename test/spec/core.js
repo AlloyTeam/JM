5bc867666fc32ea952fa02a5568ca315e0e0ff0c
yuanyan <yuanyan.cao@gmail.com>
2014-02-26 19:33:56 +0800

    添加核心属性测试

--- test/spec/core.js
+++ test/spec/core.js
@@ -9,7 +9,7 @@ define(['../../src/core'], function(){
                 expect($body[0] === body).to.be.true;
             })
 
-            it('$(<JM collection>)', function(){
+            it('$(<JM object>)', function(){
                 var $body = $( $('body') );
                 var body = document.querySelector('body');
                 expect($body[0] === body).to.be.true;
@@ -24,4 +24,31 @@ define(['../../src/core'], function(){
         })
 
     })
+
+    describe('Core properties', function(){
+
+        describe('$obj', function(){
+
+            it('$obj.version', function(){
+                var $body = $('body');
+                expect($body.version).to.equal('@VERSION');
+                $body.version = 'changed';
+                expect($body.version).to.equal('@VERSION');
+            })
+
+            it('$obj.selector', function(){
+                var $body = $('body');
+                expect($body.selector).to.equal('body');
+            })
+
+            it('$obj.context', function(){
+                var $body = $('body');
+                expect($body.context === document ).to.be.true;
+            })
+
+        })
+
+    })
+
+
 })
